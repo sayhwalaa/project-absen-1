552380a2a1b9f7ec5e1c4cdefa60158f2eeba702
rzqsaputra <saputrarizky566@gmail.com>
2022-12-05 15:09:35 +0700

    New File

---
+++ resources/views/Template/script.blade.php
@@ -0,0 +1,9 @@
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{asset('SBAdmin/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('SBAdmin/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{asset('SBAdmin/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{asset('SBAdmin/js/sb-admin-2.min.js')}}"></script>
