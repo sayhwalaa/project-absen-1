84fca05562096a418823d1c5a6c58bd50b7479a9
sayhwalaa <80562405+sayhwalaa@users.noreply.github.com>
2022-12-14 16:09:32 +0700

    New Migration

---
+++ database/migrations/2022_12_12_140240_create_levels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLevelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('level', function (Blueprint $table) {
+            $table->id();
+            // $table->foreignId('level_id');
+            $table->string('role');
+            $table->char('username');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('level');
+    }
+}
---
+++ database/migrations/2022_12_12_140308_create_presensis_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePresensisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('presensi', function (Blueprint $table) {
+            $table->id();
+            // $table->foreignId('presensi_id');
+            $table->date('tglPresensi');
+            $table->timestamp('jamMasuk');
+            $table->timestamp('jamPulang');
+            $table->text('keterangan');
+            $table->binary('foto')->nullable();
+            $table->string('lokasi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('presensi');
+    }
+}
